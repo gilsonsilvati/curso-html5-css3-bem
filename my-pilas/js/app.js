3a22ccd3bce082ea49ab5aaf36b017f727d660ad
Gilson Silva <gilsonsilvati@gmail.com>
2017-11-04 11:42:45 -0200

    JavaScript Hook: chaveando o menu.

---
+++ my-pilas/js/app.js
@@ -0,0 +1,7 @@
+/* Função para o botão chaveador (Responsivo) */
+var alternador = document.querySelector('.js-botao-chaveador');
+
+alternador.onclick = function () {
+	var menu = document.querySelector('.js-menu');
+	menu.classList.toggle('menu--exibindo');
+}
